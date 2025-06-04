712f7de1ef6ee105c2b07b5f172a52dec88d6ea2
Gegham Zakaryan <zakaryan.2004@outlook.com>
2025-06-04 14:12:16 +0400

    feat(index): Add the boilerplate frontend back at / endpoint
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import initializeDatabase from "./db.js";
+import initializeDatabase from "./src/config/db.js";
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +24,14 @@ initializeDatabase().then((database) => {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Frontend
+app.get("/", async (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
